c60b048ad50a36d8f11f5e6d258e45fc26e50f9d
Rainbowdm <lem.feather@yandex.ua>
2019-07-03 16:54:34 +0300

    Add task1.js

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,1 +1,18 @@
-// Your code goes here
+let aax = Number(window.prompt('Add value aax', ''));
+let bbx = Number(window.prompt('Add value bbx', ''));
+let ccx = Number(window.prompt('Add value ccx', ''));
+
+let aay = Number(window.prompt('Add value aay', ''));
+let bby = Number(window.prompt('Add value bby', ''));
+let ccy = Number(window.prompt('Add value ccy', ''));
+
+const num = 2;
+
+let xc = (aax + bbx) / num;
+let yc = (aay + bby) / num;
+
+if (xc === ccx && yc === ccy) {
+    console.log('c divides AB segment by half - ' + true);
+} else {
+    console.log('c doesnt device AB segment by half - ' + false);
+}
