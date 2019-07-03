766b45a1ec3e0b7b815d9e3b9b229721d52c9cab
Rainbowdm <lem.feather@yandex.ua>
2019-07-03 18:01:06 +0300

    refactor

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,9 +1,8 @@
 let aax = Number(window.prompt('Add value aax', ''));
-let bbx = Number(window.prompt('Add value bbx', ''));
-let ccx = Number(window.prompt('Add value ccx', ''));
-
 let aay = Number(window.prompt('Add value aay', ''));
+let bbx = Number(window.prompt('Add value bbx', ''));
 let bby = Number(window.prompt('Add value bby', ''));
+let ccx = Number(window.prompt('Add value ccx', ''));
 let ccy = Number(window.prompt('Add value ccy', ''));
 
 const num = 2;
