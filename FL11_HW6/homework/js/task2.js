e608a831c0d8ebf0106ce1c56c911d6b1517805b
Rainbowdm <lem.feather@yandex.ua>
2019-07-04 17:08:11 +0300

    add task2

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,1 +1,16 @@
-// Your code goes here
+let a = Number(window.prompt('a = ', ''));
+let b = Number(window.prompt('b = ', ''));
+let c = Number(window.prompt('c = ', ''));
+
+if (a + b > c && b + c > a && a + c > b) {
+    console.log('Triangle exists');
+    if (a === b && b === c && c === a) {
+        console.log('Eequivalent triangle');
+    } else if (a === b && b !== c || b === c && b !== a || a === c && b !== c) {
+        console.log('Isosceles triangle');
+    } else if (a !== b && b !== c && c !== a) {
+        console.log('Normal triangle');
+    }
+} else {
+    console.log('Triangle doesn’t exist');
+}
