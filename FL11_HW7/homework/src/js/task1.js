60d607008f6bd81b1b55dcaa166c3ad755098070
Rainbowdm <lem.feather@yandex.ua>
2019-07-08 16:38:23 +0300

    Write Step 1, 2

--- FL11_HW7/homework/src/js/task1.js
+++ FL11_HW7/homework/src/js/task1.js
@@ -1,1 +1,25 @@
-// Your code goes here
+const checkEmail = prompt('Enter your email address', '');
+const minSymbols = 6;
+let password = '';
+
+if (checkEmail === '' || checkEmail === null) {
+    alert('Canceled');
+} else if (checkEmail.length < minSymbols) {
+    alert('I don’t know any emails having name length less than 6 symbols');
+} else if (checkEmail === 'user@gmail.com' || checkEmail === 'admin@gmail.com') {
+    password = prompt('Enter your password', '');
+} else {
+    alert('I don’t know you');
+}
+
+if (password === '' || password === null) {
+    alert('Canceled');
+}
+if (checkEmail === 'user@gmail.com' && password === 'UserPass') {
+    console.log('valid password');
+    if (checkEmail === 'admin@gmail.com' && password === 'AdminPass') {
+        console.log('valid password');
+    }
+} else {
+    alert('Wrong password');
+}
